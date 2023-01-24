b9e1bde5ce1205c24bc7b81c309985295c245e76
HW4 <aliamachka@hotmail.com>
2023-01-24 10:13:36 -0800

    hw part3

--- src/Car.java
+++ src/Car.java
@@ -7,12 +7,36 @@ public class Car {
     private String country;
 
     public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+        if (brand == null || brand.isEmpty()) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null || model.isEmpty()) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null || color.isEmpty()) {
+            this.color = "white";
+        } else {
+            this.color = color;
+        }
+        if (year <= 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+        if (country == null || country.isEmpty()) {
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
     }
 
     public String getBrand() {
@@ -41,6 +65,6 @@ public class Car {
 
     @Override
     public String toString() {
-        return "Brand: " + brand + ", Model: " + model + ", Engine volume: " + engineVolume + "L, Color: " + color + ", Year: " + year + ", Built in: " + country;
+        return brand + " " + model + ", " + year + " year, built in " + country + ", color: " + color + ", engine volume: " + engineVolume + " L";
     }
 }
