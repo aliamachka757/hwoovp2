50b292c1eae29facf94a7fcc453117950f8b2cda
HW4 <aliamachka@hotmail.com>
2023-01-24 10:03:55 -0800

    hw done

---
+++ src/Car.java
@@ -0,0 +1,46 @@
+public class Car {
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand: " + brand + ", Model: " + model + ", Engine volume: " + engineVolume + "L, Color: " + color + ", Year: " + year + ", Built in: " + country;
+    }
+}
